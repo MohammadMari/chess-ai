315a33686419d64a80d1599714b4a31dd75b5022
jss0356 <114105096+jss0356@users.noreply.github.com>
2022-11-04 13:04:20 -0500

    Update ChessPiece.cpp
    Gives some functions to update the pieces valid moves container and also to load textures for that piece.

--- ChessAI/ChessPiece.cpp
+++ ChessAI/ChessPiece.cpp
@@ -1,6 +1,7 @@
 #include "ChessPiece.h"
 #include <assert.h>
 
+/*
 ChessPiece::ChessPiece()
 {
 	pieceSide = NONE;
@@ -20,12 +21,148 @@ void ChessPiece::SetSide(SIDE side)
 	pieceSide = side;
 }
 
-Texture ChessPiece::GetTexture()
-{
-	return texture;
+*/
+
+
+//adds given string into the possibleMoves container.
+void ChessPiece::add(string newPossibleMove) {
+	this->possibleMoves.push_back(newPossibleMove);
+	return;
+}
+
+//true if potentialMove found, else false if not.
+bool ChessPiece::search(string potentialMove) {
+
+	//iterates through container.
+	for (unsigned int i = 0; i < this->possibleMoves.size(); i++) {
+		//if possible move (found in container) then return true.
+		if (this->possibleMoves.at(i) == potentialMove) {
+			return true;
+		}
+	}
+	//else, not a possible move so return false.
+	return false;
+}
+
+//empties the entire possibleMoves container.
+void ChessPiece::erase() {
+	this->possibleMoves.clear();
+	return;
+}
+
+//returns the current size of the possibleMoves container.
+int ChessPiece::size() {
+	return this->possibleMoves.size();
+}
+
+//sets the texture for the specified chess piece instance.
+void ChessPiece::loadTexture() {
+	//if the color associated with the game piece is white, load from white pool of textures.
+	if (this->pieceColor == "White") {
+		//if this is true, we need to load a white pawn texture.
+		if (this->pieceName == "Pawn") {
+			//if the texture cannot be found, print associated error and exit.
+			if (!texture.loadFromFile("sprites/pawnw.png")) {
+				printf("pawnw error\n");
+				exit(0);
+			}
+		}
+
+		if (this->pieceName == "Horse") {
+			//if the texture cannot be found, print associated error and exit.
+			if (!texture.loadFromFile("sprites/knightw.png")) {
+				printf("horsew error\n");
+				exit(0);
+			}
+		}
+
+		if (this->pieceName == "Rook") {
+			//if the texture cannot be found, print associated error and exit.
+			if (!texture.loadFromFile("sprites/rookw.png")) {
+				printf("rookw error\n");
+				exit(0);
+			}
+		}
+
+		if (this->pieceName == "Bishop") {
+			//if the texture cannot be found, print associated error and exit.
+			if (!texture.loadFromFile("sprites/bishopw.png")) {
+				printf("bishopw error\n");
+				exit(0);
+			}
+		}
+
+		if (this->pieceName == "King") {
+			//if the texture cannot be found, print associated error and exit.
+			if (!texture.loadFromFile("sprites/kingw.png")) {
+				printf("kingw error\n");
+				exit(0);
+			}
+		}
+
+		if (this->pieceName == "Queen") {
+			//if the texture cannot be found, print associated error and exit.
+			if (!texture.loadFromFile("sprites/queenw.png")) {
+				printf("queenw error\n");
+				exit(0);
+			}
+		}
+	}
+	//else, must be black so load from black pool of textures.
+	else {
+		if (this->pieceName == "Pawn") {
+			//if the texture cannot be found, print associated error and exit.
+			if (!texture.loadFromFile("sprites/pawnb.png")) {
+				printf("pawnb error\n");
+				exit(0);
+			}
+		}
+
+		if (this->pieceName == "Horse") {
+			//if the texture cannot be found, print associated error and exit.
+			if (!texture.loadFromFile("sprites/knightb.png")) {
+				printf("knightb error\n");
+				exit(0);
+			}
+		}
+
+		if (this->pieceName == "Rook") {
+			//if the texture cannot be found, print associated error and exit.
+			if (!texture.loadFromFile("sprites/rookb.png")) {
+				printf("rookb error\n");
+				exit(0);
+			}
+		}
+
+		if (this->pieceName == "Bishop") {
+			//if the texture cannot be found, print associated error and exit.
+			if (!texture.loadFromFile("sprites/bishopb.png")) {
+				printf("bishopb error\n");
+				exit(0);
+			}
+		}
+
+		if (this->pieceName == "King") {
+			//if the texture cannot be found, print associated error and exit.
+			if (!texture.loadFromFile("sprites/kingb.png")) {
+				printf("kingb error\n");
+				exit(0);
+			}
+		}
+
+		if (this->pieceName == "Queen") {
+			//if the texture cannot be found, print associated error and exit.
+			if (!texture.loadFromFile("sprites/queenb.png")) {
+				printf("queenb error\n");
+				exit(0);
+			}
+		}
+	}
+
 }
 
 
+/*
 // Rook
 Rook::Rook(SIDE side)
 {
@@ -152,3 +289,5 @@ Knight::Knight(SIDE side)
 		exit(0);
 	}
 }
+
+*/
