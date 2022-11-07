219c28b84e6dcf4f4e42a6ffcb8c5234b020d50c
jss0356 <114105096+jss0356@users.noreply.github.com>
2022-11-06 23:15:19 -0600

    Update ChessPiece.h

--- ChessAI/ChessPiece.h
+++ ChessAI/ChessPiece.h
@@ -10,6 +10,18 @@ enum SIDE {
 	BLACK
 };
 
+
+//defines general piece type.
+enum PIECE_TYPE {
+	ROOK,
+	KNIGHT,
+	BISHOP,
+	QUEEN,
+	KING,
+	PAWN,
+	INVALID
+};
+
 using namespace sf;
 
 struct Pos {
@@ -34,11 +46,30 @@ public:
 	void SetX(int x);
 	void SetY(int y);
 	string TypeLabel;
+
+
+
+
+	//has the piece been moved yet? Intially false, and currently only implemented for double step for pawns.
+	bool isMoved = false;
+
+	//return defined piece type.
+	PIECE_TYPE getPieceType();
+
+	bool getCheckStatusForPiece();
+	void setCheckStatusForPiece(bool valueToSet);
+
 	bool ProtectingQueen() {
 		return false; // temporary
 	}
-	virtual vector<Pos> PossibleMoves(ChessPiece*** board) = 0;
+	virtual vector<Pos> PossibleMoves(ChessPiece*** board, SIDE pieceColor = NONE) = 0;
 protected:
+	//this determines whether if a given piece is currently checking enemy King or not.
+	bool isPiecePartCheck = false;
+
+	//label to identify general type of piece.
+	PIECE_TYPE pieceName = INVALID;
+
 	SIDE pieceSide = NONE;
 	Texture texture;
 	Pos pos = Pos(0, 0);
@@ -48,7 +79,7 @@ protected:
 class Empty : public ChessPiece {
 public:
 	Empty(Pos pos, string label);
-	vector<Pos> PossibleMoves(ChessPiece*** board) {
+	vector<Pos> PossibleMoves(ChessPiece*** board, SIDE pieceColor = NONE) {
 		return vector<Pos>();
 	}
 };
@@ -57,37 +88,37 @@ public:
 class Rook : public ChessPiece {
 public:
 	Rook(SIDE side, Pos pos, string label);
-	vector<Pos> PossibleMoves(ChessPiece*** board);
+	vector<Pos> PossibleMoves(ChessPiece*** board, SIDE pieceColor = NONE);
 };
 
 class Bishop : public ChessPiece {
 public:
 	Bishop(SIDE side, Pos pos, string label);
-	vector<Pos> PossibleMoves(ChessPiece*** board);
+	vector<Pos> PossibleMoves(ChessPiece*** board, SIDE pieceColor = NONE);
 };
 
 class King : public ChessPiece {
 public:
 	King(SIDE side, Pos pos, string label);
-	vector<Pos> PossibleMoves(ChessPiece*** board);
+	vector<Pos> PossibleMoves(ChessPiece*** board, SIDE pieceColor = NONE);
 };
 
 class Queen : public ChessPiece {
 public:
 	Queen(SIDE side, Pos pos, string label);
-	vector<Pos> PossibleMoves(ChessPiece*** board);
+	vector<Pos> PossibleMoves(ChessPiece*** board, SIDE pieceColor = NONE);
 };
 
 class Knight : public ChessPiece {
 public:
 	Knight(SIDE side, Pos pos, string label);
-	vector<Pos> PossibleMoves(ChessPiece*** board);
+	vector<Pos> PossibleMoves(ChessPiece*** board, SIDE pieceColor = NONE);
 };
 
 class Pawn : public ChessPiece {
 public:
 	Pawn(SIDE side, Pos pos, string label);
-	vector<Pos> PossibleMoves(ChessPiece*** board);
+	vector<Pos> PossibleMoves(ChessPiece*** board, SIDE pieceColor = NONE);
 private:
 	bool Moved = false; // used for en passant
 };
